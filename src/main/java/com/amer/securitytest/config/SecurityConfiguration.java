5d81a7e37d0b8a5fc98592d044479c58c5608ac7
Amer Salah Ali <asalah@citglobal.com>
2019-10-30 16:02:09 +0200

    Applying Spring Security with JDBC Default Schema

--- src/main/java/com/amer/securitytest/config/SecurityConfiguration.java
+++ src/main/java/com/amer/securitytest/config/SecurityConfiguration.java
@@ -1,31 +1,57 @@
 package com.amer.securitytest.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
+    @Autowired
+    DataSource dataSource;
+
+
     //Used for Authentication
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        //set Configuration to auth Object
-        auth.inMemoryAuthentication().withUser("Amer")
-                                     .password("123456")
-                                        .roles("USER")
-                .and()
-                .withUser("Ahmed")
-                .password("987654")
-                .roles("ADMIN");
+        //In Memory Database
+
+//        //set Configuration to auth Object
+//        auth.inMemoryAuthentication().withUser("Amer")
+//                                     .password("123456")
+//                                        .roles("USER")
+//                .and()
+//                .withUser("Ahmed")
+//                .password("987654")
+//                .roles("ADMIN");
+
+        //H2 Database
+
+        auth.jdbcAuthentication().dataSource(dataSource);
+//                .withDefaultSchema()
+//                .withUser(
+//
+//                        User.withUsername("User")
+//                        .password("pass")
+//                        .roles("USER")
+//                )
+//                .withUser(
+//                        User.withUsername("Admin")
+//                                .password("pass")
+//                                .roles("ADMIN")
+//                );
     }
 
 
